2459bceff97160fc747cad03b23736ff37d05e20
mirellaor <mirellaor>
2023-08-20 18:39:31 -0300

    função pra encontrar o proprietario pelo id

--- src/controladores/proprietario.js
+++ src/controladores/proprietario.js
@@ -2,6 +2,7 @@ let dadosProprietario = [];
 let id = 1;
 
 function criarProprietario(req, res) {
+    //criando novos proprietários e adicionando o id no inicio dos dados:
     const novoProprietario = { id, ...req.body };
     id++;
     dadosProprietario.push(novoProprietario);
@@ -12,8 +13,21 @@ function listarProprietario(req, res) {
     res.status(200).json(dadosProprietario);
 }
 
+function listarProprietarioId(req, res) {
+    const { id } = req.params;
+    const proprietarioEncontrado = dadosProprietario.find((item) => item.id === Number(id));
+
+    if (!proprietarioEncontrado) {
+        res.status(404).json({ mensagem: "usuário não encontrado." });
+    }
+
+    res.status(200).json(proprietarioEncontrado);
+
+}
+
 module.exports =
 {
     criarProprietario,
-    listarProprietario
+    listarProprietario,
+    listarProprietarioId
 }
