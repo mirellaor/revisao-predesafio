61aeedf4285d08d2bb5d93be5805d891e51e97f4
mirellaor <mirellaor>
2023-08-20 12:27:30 -0300

    criando as rotas pra criar e listar os proprietarios dos pets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const proprietario = require('./controladores/proprietario');
 
 const app = express();
+app.use(express.json());
 
-
+app.post('/proprietario', proprietario.criarProprietario);
+app.get('/proprietario', proprietario.listarProprietario);
 
 app.listen(3000);
