0c64b6cef40a0e1777771431a08018cb32495240
mirellaor <mirellaor>
2023-08-20 18:39:17 -0300

    rota pra encontrar o proprietario pelo id

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ app.use(express.json());
 
 app.post('/proprietario', proprietario.criarProprietario);
 app.get('/proprietario', proprietario.listarProprietario);
+//na rota abaixo, depois dos dois pontos (:), é o que vai ser armazenado quando rodamos a requisição la no insomnia depois de proprietarios
+app.get('/proprietario/:id', proprietario.listarProprietarioId);
 
 app.listen(3000);
