8abb8c3e73e845aa6b5ab1c2f2a4bd7e3f710fb2
mirellaor <mirellaor>
2023-08-20 22:24:26 -0300

    att rotas

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/proprietario', proprietario.criarProprietario);
-app.get('/proprietario', proprietario.listarProprietario);
+app.get('/proprietario', proprietario.listarProprietarioNome);
 //na rota abaixo, depois dos dois pontos (:), é o que vai ser armazenado quando rodamos a requisição la no insomnia depois de proprietarios
 app.get('/proprietario/:id', proprietario.listarProprietarioId);
 
